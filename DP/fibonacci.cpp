3b31b8a1fa5dc4b8cdff92b98a124ad8dbe42c81
Darshan <darshankok46@gmail.com>
2026-03-03 13:32:50 +0530

    DP started

---
+++ DP/fibonacci.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int fibonacciDP(int n,vector<int> &f){
+    //BC
+    if( n == 0 || n == 1){
+        return n;
+    }
+
+    if(f[n] != -1){
+        return f[n];
+    }
+
+    f[n] = fibonacciDP(n-1,f) + fibonacciDP(n-2,f);
+    return f[n];
+}
+
+
+int main (){
+
+    int n = 6;
+    vector<int> f(n+1,-1);
+    cout << fibonacciDP(n,f) << endl;
+
+    return 0;
+}
