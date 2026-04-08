c70b47989c937c3d3523abde67e0d54c61234d59
Darshan R <darshankok7@gmail.com>
2026-04-08 21:24:42 +0530

    longest comman subsequence sub string in memiozation and tabulation

--- DP/3/string.cpp
+++ DP/3/string.cpp
@@ -44,6 +44,27 @@ int lcsMem(string str1,string str2,vector<vector<int>> dp){  //0(n * m)
 }
 
 
+int lcsTab(string str1,string str2){
+   
+    int n = str1.size();
+    int m = str2.size();
+
+    vector<vector<int>> dp(n+1,vector<int>(m+1,0)); 
+
+
+    for(int i=1;i<n+1;i++){
+        for(int j=1;j<m+1;j++){
+            if(str1[i-1] == str2[j-1]){
+                dp[i][j] = 1 + dp[i-1][j-1]; // 1 is like count how many times valid comman subseq are present
+            }else{
+                dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+    }
+    return dp[n][m];
+}
+
+
 int main() {
 
     string str1 = "abcdgefghijkmqrstuv";
@@ -56,6 +77,8 @@ int main() {
     cout << "longest comman subsequence is " << lsRec(str1,str2) << endl;
 
     cout << "longest comman subsequence Mem " << lcsMem(str1,str2,dp) << endl;
+
+    cout << "longest comman subsequence Tab " << lcsTab(str1,str2) << endl;
 
 
     return 0;
