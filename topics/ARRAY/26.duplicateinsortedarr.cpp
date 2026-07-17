1e73f8a818ff1be8c381b65be26646b3faa4780d
darsh73an <darshankok7@gmail.com>
2026-07-17 19:24:17 +0530

    continue dsa in leetcode

--- topics/ARRAY/26.duplicateinsortedarr.cpp
+++ topics/ARRAY/26.duplicateinsortedarr.cpp
@@ -3,6 +3,30 @@ public:
     int removeDuplicates(vector<int>& nums) {
         int n = nums.size();
 
+        int i=0; // i moves or counts till the unique elements or points till unique elements in nums
+
+        // i for counting unique elements
+        // j iterates and checks for other unique elements
+
+       for(int j=0; j<n; j++){
+        if(nums[i] != nums[j]){   // means unique element
+            i++;
+        }
+
+            nums[i] = nums[j];
+       }
+       return i+1; // bcoz idx starts with 0 and i counts thew no of unique elements
+    }
+};
+
+
+
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int n = nums.size();
+
         int i=0; // i moves till the unique elements or points till unique elements in nums
 
         // i for counting unique elements
