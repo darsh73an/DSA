4d149964fcfd33a91e7367fae4869988ea8a0db4
darsh73an <darshankok7@gmail.com>
2026-08-17 05:54:17 +0530

    stared binarys search

---
+++ topics/BS/34.findFirstAndLastPositionsOfSameElementInarr.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int n = nums.size();
+        int l = 0;
+        int r = n-1;
+        int first = -1;
+
+        // first find any occurance then go left if another found then reassign it to fist occurance variable same for last occurance
+        while(l <= r){
+            int m = l + (r-l) / 2;
+
+            if(nums[m] == target){
+                first = m;
+                r = m - 1;
+            }else if(nums[m] > target){
+                r = m-1;
+            }else{
+                l = m+1;
+            }
+        }
+
+        l = 0;
+        r = n-1;
+        int last = -1;
+
+        while(l <= r){
+            int m = l + (r-l)/2;
+
+            if(nums[m] == target){
+                last = m;
+                l = m+1;
+            }else if(nums[m] > target){
+                r = m-1;
+            }else{
+                l = m+1;
+            }
+        }
+        return {first,last};
+    }
+};
