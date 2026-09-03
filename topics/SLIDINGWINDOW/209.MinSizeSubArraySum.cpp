082ed2e60ed2d0332f8eb424ae78e452a2bf3bac
darsh73an <darshankok7@gmail.com>
2026-09-03 05:50:27 +0530

    sloved min len which >= target in +arr

---
+++ topics/SLIDINGWINDOW/209.MinSizeSubArraySum.cpp
@@ -0,0 +1,22 @@
+// We need to find the minLen of subarr thats makes value >= target
+class Solution {
+public:
+    int minSubArrayLen(int target, vector<int>& nums) {
+        int n = nums.size();
+        int left = 0;
+        int minLen = INT_MAX;
+        int sum = 0;
+
+        for(int right=0; right<n; right++){
+            sum += nums[right];
+
+            while(sum >= target){
+                minLen = min(minLen,right-left+1);
+
+                sum -= nums[left];
+                left++;
+            }
+        }
+        return minLen == INT_MAX ? 0 : minLen;
+    }
+};
