462a50e276fde6c3cb45725ddd55e3d730da097e
darsh73an <darshankok7@gmail.com>
2026-09-04 06:00:25 +0530

    permuatation in string learned and sloved using sliding window pattern

---
+++ topics/SLIDINGWINDOW/567.PermuattaionInString.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        if(s1.size() > s2.size()) return false;
+
+        vector<int>freq1(26,0) ,freq2(26,0);
+
+        for(char c : s1){
+            freq1[c - 'a']++;
+        }
+
+        int k = s1.size();   // window size 
+        for(int i=0; i<s2.size(); i++){
+            freq2[s2[i] - 'a']++;
+
+            if(i >= k){
+                freq2[s2[i-k] - 'a']--;  // to move to nxt window remove first char 
+            }
+            if(freq1 == freq2) return true;
+        }
+        return false;
+    }
+};
