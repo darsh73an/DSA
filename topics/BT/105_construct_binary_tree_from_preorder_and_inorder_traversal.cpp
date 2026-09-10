2e4f08e2b96f2ddafc84d2d37b93c2460fad27d0
darsh73an <darshankok7@gmail.com>
2026-09-11 05:12:57 +0530

    completed build tree form inorder and preorder

---
+++ topics/BT/105_construct_binary_tree_from_preorder_and_inorder_traversal.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int search(vector<int>& inorder, int left, int right, int preRoot){
+        for(int i=left; i<=right; i++){
+            if(preRoot == inorder[i]){
+                return i;
+            }
+        }
+        return -1; // this will not happen bcoz they have said inorder and preorder is valid
+    }
+
+    TreeNode* helper(vector<int>& preorder, vector<int>& inorder, int left, int right, int& preIdx){
+        // for bc null values
+        if(left > right){  // the idx in inOrder
+            return nullptr;
+        }
+
+        TreeNode* root = new TreeNode(preorder[preIdx]);  // we need to send int val not TreeNode* root
+
+        int inIdx = search(inorder,left,right,preorder[preIdx]);  // after search only increment prreIdx 
+        preIdx++;
+
+        root->left = helper(preorder,inorder,left,inIdx-1,preIdx);
+        root->right = helper(preorder,inorder,inIdx+1,right,preIdx);
+
+        return root;
+    }
+
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        int preIdx = 0;
+        return helper(preorder,inorder,0,inorder.size()-1,preIdx);
+    }
+};
